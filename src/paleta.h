0cb89ccb136f19d5a4105c20bb15a3c05afc4dc9
edvaldinhs <edvaldohenriqueceu@gmail.com>
2025-07-04 03:08:13 -0300

    #1 fix: lerArquivo() completo

--- src/paleta.h
+++ src/paleta.h
@@ -1,12 +1,7 @@
 #include <string>
 #include <fstream>
 #include <vector>
-
-//Oi Luis
-//Fiz metade do que se pedia na paleta
-//Devido minha péssima memória,
-//me lembre de discutir como fazer a função lerArquivo()
-
+#include <sstream>
 
 
 struct Cor {
@@ -39,56 +34,50 @@ class Paleta {
     Cor getCor(int valores){
         return cores[valores];
     }
-    void lerArquivo(std::string arquivoTexto){
+    void lerArquivo(std::string arquivoTexto) {
         std::ifstream file(arquivoTexto);
-        if (file.is_open()) {
-            std::string textoFinal;
-            while (getline(file, arquivoTexto)) {
-                textoFinal += arquivoTexto + '\n';
-            }
-            std::cout << textoFinal;
-            bool nGet = false;
-            std::string qtd = "";
-            int x = 0;
-            std::vector<int> v;
-            std::vector<int> w;
-            std::string vInt = "", wInt = "";
-            for (int i = 0; i < textoFinal.size(); i++) {
-                if (!nGet && textoFinal [i] != '\n') {
-                    qtd += textoFinal[i];
-                }
-                else {
-                    nGet = true;
-                    i++;
-                }
-                if (nGet) {
-                    if (x == 0 && textoFinal [i] != ' ') {
-                        vInt += textoFinal [i];
-                    }
-                    else {
-                        vInt = "";
-                        x++;
-                    }
-                    if (x > 0 && x <= 3 && textoFinal [i] != ' ' && textoFinal [i] != '\n') {
-                        wInt += textoFinal [i];
-                    }
-                    else {
-                        wInt = "";
-                        x++;
-                    }
-                    if (x > 3) {
-                        x = 0;
-                    }
-                }
-            }
-            // v.push_back(stoi(vInt));
-            quantidade = stoi(qtd);
-            std::cout << vInt << std::endl;
-            std::cout << wInt << std::endl;
-        }
-        else {
+        if (!file.is_open()) {
             std::cout << "Erro de abertura" << std::endl;
+            return;
         }
+
+        std::string linha;
+        
+        if (std::getline(file, linha)) {
+            quantidade = std::stoi(linha);
+        }
+
+        int i = 0;
+    while (std::getline(file, linha) && i < 100 && i < quantidade) {
+        int valor = 0, r = 0, g = 0, b = 0;
+        int pos = 0;
+        int inicio = 0;
+        int camposLidos = 0;
+
+        for (int j = 0; j <= linha.size(); ++j) {
+            if (j == linha.size() || linha[j] == ' ') {
+                std::string parte = linha.substr(inicio, j - inicio);
+                if (!parte.empty()) {
+                    int numero = std::stoi(parte);
+                    switch (camposLidos) {
+                        case 0: valor = numero; break;
+                        case 1: r = numero; break;
+                        case 2: g = numero; break;
+                        case 3: b = numero; break;
+                    }
+                    camposLidos++;
+                }
+                inicio = j + 1;
+            }
+        }
+
+        if (camposLidos == 4) {
+            valores[i] = valor;
+            cores[i] = {r, g, b};
+            i++;
+        }
+    }
+
         file.close();
     }
 };
--- teste/testepaleta.cpp
+++ teste/testepaleta.cpp
@@ -18,3 +18,11 @@ TEST_CASE("Consultar cor por altura retorna a cor esperada") {
     CHECK(paleta.getCor(2) == Cor{0, 0, 255});
 }
 
+TEST_CASE("Ler arquivo") {
+    Paleta paleta("../paleta.ppm");
+
+    CHECK(paleta.getCor(0) == Cor{255, 0, 0});
+    CHECK(paleta.getCor(1) == Cor{0, 255, 0});
+    CHECK(paleta.getCor(2) == Cor{0, 0, 255});
+}
+
